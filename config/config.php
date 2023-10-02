3912f7c645330aefd029201332465a9da0289e44
Md.Meherab Hossain <97660818+meherab14@users.noreply.github.com>
2023-10-02 23:46:14 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "id21338816_blog20cse038");
+define("DB_PASS", "Meher123@");
+define("DB_NAME", "id21338816_db_blog");
+define("TITLE", "Training With Live Project");
+define("KEYWORDS", "PHP Tutorials, JAVA Tutorials, Oracle Database, C# Tutorials");
+
